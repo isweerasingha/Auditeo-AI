a6a599594f6da8166f5fac9d89f42f000da21eeb
Isuru Weerasingha <info.isweerasingha@gmail.com>
2026-03-19 14:06:14 +0530

    Refactor import statements in utils module for improved readability and maintainability.

--- src/auditeo_ai/utils/__init__.py
+++ src/auditeo_ai/utils/__init__.py
@@ -1,4 +1,4 @@
-from auditeo_ai.utils.format_url import format_url
-from auditeo_ai.utils.generate_report import generate_pdf_report
+from .format_url import format_url
+from .generate_report import generate_pdf_report
 
 __all__ = ["generate_pdf_report", "format_url"]
